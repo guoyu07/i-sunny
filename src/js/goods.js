140893f4c730a6d9fe38aae4da680c72797d1a1f
linzixi31 <912379914@qq.com>
2017-11-19 22:29:17 +0800

    放大镜的内容

--- src/js/goods.js
+++ src/js/goods.js
@@ -1,8 +1,30 @@
 require(['config'],function(){
     require(['jquery','common'],function($){       
 
-        $('#pageheader').load("./header.html");
+        $('#pageheader').load("./header.html",function(){
+             $('.header_b').on('mouseenter','dd',function(e){
+                        $(this).find('.shopcont').css({'display':'block'})
+                    //console.log($(this).find('.shopcont'));
+                    });
+                    $('.header_b').on('mouseleave','dd',function(e){
+                        $(this).find('.shopcont').css({'display':'none'})
+                    })
+                    $('#nav').on('click','a',function(e){
+                    //console.log(666)
+                    /*-----点击跳转页面#list---*/
+                        console.log($(this)[0]);
+                        if($(this)[0].innerText ==='首页'){
+                            $(this)[0].href= '../index.html';
+                        }else{
+                            //除首页外跳转list.html
+                            var cary = $(this)[0].innerText;
+                            location.href= './list.html?category='+cary;
+
+                        }
+                    })
+        });
         $('#pagefooter').load("./footer.html");
+        
         /*-----连接首尾-----*/
         var zf  = location.search;
         zf=zf.slice(1);
@@ -74,7 +96,63 @@ require(['config'],function(){
             //location连接shopping_car.html
             location.href = '../html/car.html?id='+id;
                     
-
+           
         })
+        /*-------放大镜-----*/
+
+                var move = document.querySelector(".move");
+                var img = $(".bigimg")[0];
+                var b_move = img.nextElementSibling;
+                var bImg = b_move.children[0];
+                console.log(img);
+                
+                //普通写法
+                    
+                    img.onmousemove = function(e){
+                        move.style.display = "block";
+                        e = e||window.event;
+                        var target = e.target || e.srcElement;                      
+                        
+                        //e.clientX/Y >遮罩宽高一半加上遮罩offsetWidth/Top
+                        
+                        var m_left = e.clientX - move.offsetWidth/2;
+                        var m_top = e.clientY - move.offsetHeight/2;
+                        move.style.left = m_left+"px";
+                        move.style.top = m_top+"px";
+
+                        if(e.clientX > img.offsetWidth - move.offsetWidth/2){
+                            move.style.left = img.offsetWidth - move.offsetWidth+"px";
+
+                        }
+                        if(e.clientY > img.offsetHeight - move.offsetHeight/2){
+                            move.style.top = img.offsetHeight - move.offsetHeight +"px";
+                        }
+                        
+                        if(m_left <0){
+                            move.style.left = 0;
+                        }
+                        
+                        if(m_top<0){
+                            move.style.top = 0;
+                        }
+
+                        var percent = 1024/400;
+                        b_move.style.display = "block";
+                        bImg.style.display = "block";
+                        
+                        // b_move.style.width = move.offsetWidth*percent+"px";
+                        // b_move.style.height = move.offsetHeight*percent+"px";
+                        
+                        var bImgLeft = move.offsetLeft*percent;
+                        var bImgTop = move.offsetTop*percent;
+                        
+                        bImg.style.left = -bImgLeft+"px";
+                        bImg.style.top = -bImgTop+"px";
+                    }
+                
+                img.onmouseleave = function(){
+                    move.style.display = "none";
+                    b_move.style.display = "none";
+                }
     })
 })
